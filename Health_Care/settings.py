50dff33b87316ef6a38809b39a6933756af9ebfe
rajeshmondal00 <131173899+rajeshmondal00@users.noreply.github.com>
2024-09-21 11:44:54 +0530

    Update settings.py

--- Health_Care/settings.py
+++ Health_Care/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ="eatgreat090@gmail.com"
-EMAIL_HOST_PASSWORD = "yyre znby qrlb rrcl"
+EMAIL_HOST_USER ="XXXXXXXXX@gmail.com"
+EMAIL_HOST_PASSWORD = "XXXXXXXXXXXXXXX"
